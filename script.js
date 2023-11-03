a0810bdcbc3e77f7d0bf7129a28f048d202eebee
Farukh <akbfarukh@gmail.com>
2023-11-03 22:57:28 +0600

    modified

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
-const input1 = document.querySelector('#num1');
-const input2 = document.querySelector('#num2');
-const btnSubmit = document.querySelector('#btn-submit');
+// Получаем ссылки на необходимые элементы
+const inputNumberElement = document.getElementById("inputNumber");
+const totalNumberElement = document.getElementById("totalNumber");
+const counterElement = document.getElementById("counter");
+const multiplaction = document.getElementById("multiplaction");
+const clear = document.getElementById("btn-clear");
 
+// Обработчик события для кнопки "clear"
+clear.addEventListener("click", function() {
+    inputNumberElement.textContent = "0";  // Сброс значения в inputNumber
+    totalNumberElement.textContent = "0";  // Сброс значения в totalNumber
+    numberButtons.forEach(function(btn) {
+      btn.classList.remove("active");  // Сброс активности кнопок 1, 2, 3, 4, 5
+    });
+    checkAndDisplayCounter();  // Проверка условий и отображение/скрытие counter
+  });
+  
 
-
-// btnSubmit.addEventListener('click', () => {
-//     const sum = Number(input1.value) + Number(input2.value);
-//     alert(sum)
-// })
-
-// btnSubmit.addEventListener('click', () => {
-//     const  user = {
-//         name: input1.value,
-//         age: input2.value
-//     }
-//     alert(JSON.stringify(user))
-// })
-
-btnSubmit.addEventListener('click', () => {
-    const  name = input1.value
-    const  age = input2.value
-    if (name.length >= 1 && age > 0) {
-        alert(`Welcome ${name} - your age ${age}!`)
-    } else if (name.length <= 0 && age.length > 0) {
-        alert('Please enter your name')
-    } else if (name.length >= 1 && age.length <= 0) {
-        alert('Please enter your age')
-    }
+// Обработчики событий для кнопок "deacrese" и "increase"
+document.getElementById("btn-deacrese").addEventListener("click", function() {
+  let currentNumber = parseInt(inputNumberElement.textContent);
+  currentNumber -= 1;
+  inputNumberElement.textContent = currentNumber;
+  checkAndDisplayCounter();
 });
+
+document.getElementById("btn-increase").addEventListener("click", function() {
+  let currentNumber = parseInt(inputNumberElement.textContent);
+  currentNumber += 1;
+  inputNumberElement.textContent = currentNumber;
+  checkAndDisplayCounter();
+});
+
+// Обработчик события для кнопки "multiplication"
+multiplaction.addEventListener("click", function() {
+  const selectedNumber = parseInt(document.querySelector(".btn-num.active").textContent);
+  const currentNumber = parseInt(inputNumberElement.textContent);
+  const result = selectedNumber * currentNumber;
+  totalNumberElement.textContent = result;
+  checkAndDisplayCounter();
+});
+
+// Обработчики событий для кнопок 1, 2, 3, 4, 5
+const numberButtons = document.querySelectorAll(".btn-num");
+numberButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    numberButtons.forEach(function(btn) {
+      btn.classList.remove("active");
+    });
+    button.classList.add("active");
+  });
+});
+
+// Функция для проверки условий и отображения/скрытия counter
+function checkAndDisplayCounter() {
+  const totalNumber = parseInt(totalNumberElement.textContent);
+  if (totalNumber < 0 || totalNumber > 100) {
+    counterElement.style.visibility = "visible";
+  } else {
+    counterElement.style.visibility = "hidden";
+  }
+}
+
+// Обработчик события для кнопки "Close" в counter
+document.querySelector(".btn-close").addEventListener("click", function() {
+  counterElement.style.visibility = "hidden";
+});
+
+// Инициализация: начальное скрытие counter
+counterElement.style.visibility = "hidden";
