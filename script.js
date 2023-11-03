bd7b4f40e11b065e80f7ca8cd8ea8025eaa3491e
Farukh <akbfarukh@gmail.com>
2023-11-03 23:04:16 +0600

    modified

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Получаем ссылки на необходимые элементы
 const inputNumberElement = document.getElementById("inputNumber");
 const totalNumberElement = document.getElementById("totalNumber");
 const counterElement = document.getElementById("counter");
 const multiplaction = document.getElementById("multiplaction");
 const clear = document.getElementById("btn-clear");
 
-// Обработчик события для кнопки "clear"
 clear.addEventListener("click", function() {
     inputNumberElement.textContent = "0";  // Сброс значения в inputNumber
     totalNumberElement.textContent = "0";  // Сброс значения в totalNumber
@@ -16,7 +14,6 @@ clear.addEventListener("click", function() {
   });
   
 
-// Обработчики событий для кнопок "deacrese" и "increase"
 document.getElementById("btn-deacrese").addEventListener("click", function() {
   let currentNumber = parseInt(inputNumberElement.textContent);
   currentNumber -= 1;
@@ -31,7 +28,6 @@ document.getElementById("btn-increase").addEventListener("click", function() {
   checkAndDisplayCounter();
 });
 
-// Обработчик события для кнопки "multiplication"
 multiplaction.addEventListener("click", function() {
   const selectedNumber = parseInt(document.querySelector(".btn-num.active").textContent);
   const currentNumber = parseInt(inputNumberElement.textContent);
@@ -40,7 +36,6 @@ multiplaction.addEventListener("click", function() {
   checkAndDisplayCounter();
 });
 
-// Обработчики событий для кнопок 1, 2, 3, 4, 5
 const numberButtons = document.querySelectorAll(".btn-num");
 numberButtons.forEach(function(button) {
   button.addEventListener("click", function() {
@@ -51,7 +46,6 @@ numberButtons.forEach(function(button) {
   });
 });
 
-// Функция для проверки условий и отображения/скрытия counter
 function checkAndDisplayCounter() {
   const totalNumber = parseInt(totalNumberElement.textContent);
   if (totalNumber < 0 || totalNumber > 100) {
@@ -61,10 +55,8 @@ function checkAndDisplayCounter() {
   }
 }
 
-// Обработчик события для кнопки "Close" в counter
 document.querySelector(".btn-close").addEventListener("click", function() {
   counterElement.style.visibility = "hidden";
 });
 
-// Инициализация: начальное скрытие counter
 counterElement.style.visibility = "hidden";
